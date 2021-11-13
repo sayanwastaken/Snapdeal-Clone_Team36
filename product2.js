a46d89a41c7c032b476b003ee6bd7f402ffd1151
sagarshiv13 <93374534+sagarshiv13@users.noreply.github.com>
2021-11-13 11:29:03 +0530

    added manish page

---
+++ product2.js
@@ -0,0 +1,115 @@
+
+
+/-----------------button check--------------------------------/ 
+
+document.getElementById("check").addEventListener("click", pinCode);
+ 
+ 
+ 
+
+ 
+var Div2 = document.getElementById("location")
+Div2.style.display = "none";
+
+
+
+function pinCode(){
+     var  code = document.getElementById("Pin").value;
+     var x_code = document.getElementById("pin1")
+     var para = document.getElementById("cng");
+     var h1 = document.createElement("h1");
+     h1.textContent = "Item is available at"
+     var y =  h1.textContent + " " + code  + ".";
+      
+     
+     
+     
+      
+     
+    var Div1 = document.getElementById("checkAddress")
+     if(code == '' || code.length <6 || code.length > 6){
+        x_code.textContent = "Please enter a valid pin Code"
+     }else if(  code.length == 6){
+         Div2.style.display = "block";
+         x_code.textContent = "";
+         Div1.style.display = "none";
+         para.textContent = y
+         
+         //Div2.append("para.textContent")
+         
+          
+         
+     } 
+
+   //  Div1.style.display = "block"
+    }
+     
+
+
+
+ document.getElementById("chg").addEventListener("click", pin_Code);
+
+ function pin_Code(){
+     var Div1 = document.getElementById("checkAddress")
+     var Div2 = document.getElementById("location")
+     
+      if(Div2.style.display == "none"){
+       Div2.style.display = "block";
+      }else{
+          Div2.style.display = "none"
+      }
+
+      Div1.style.display = "block"
+
+ }
+/--------------------cart--------------------------/
+var cartproduct = [
+     
+
+    {
+        image_url:"https://n2.sdlcdn.com/imgs/b/x/0/Heaven-D-cor-Metal-Laxmi-SDL709086684-1-abe57.jpg",
+    
+
+        price: 499,
+        pprice:209,
+
+        typeof:" Hicode Multi Ankle Length Socks Pack of 6"
+        }
+
+         
+]
+
+
+
+//localStorage.setItem("CartData", JSON.stringify(cartproduct));
+//var array = []
+
+ var array = JSON.parse(localStorage.getItem("CartData")) || []
+
+  
+ document.getElementById("Mcart").addEventListener("click", manishbutton)
+
+ 
+     
+      
+    
+    
+    
+     
+
+
+ function manishbutton(){
+     cartproduct.map(function(item){
+                   var obj = {
+                      image: item.image_url,
+                      pprice: item.pprice,
+                      price:item.price,
+                      type:item.typeof,
+         
+                    }
+         
+                    array.push(obj)
+                    localStorage.setItem("CartData", JSON.stringify(array));
+ })
+ }
+ 
